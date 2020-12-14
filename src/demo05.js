d9af47d5c25a07ec9186717d19e64becf8966aeb
Madhu Srinivasan <msrinivasa@cray.com>
2020-12-14 14:03:32 -0800

    changed array of arrays to array of objects

--- src/demo05.js
+++ src/demo05.js
@@ -3,8 +3,30 @@ import * as d3 from "d3";
 
 
 function makeDemo() {
-  let ds1 = [["Mary", 1], ["Jane", 4], ["Anne", 2]];
-  let ds2 = [["Anne", 5], ["Jane", 3]];
+  let ds1 = [
+          {
+                  "name": "Mary",
+                  "value": 1
+          },
+          {
+                  "name": "Jane",
+                  "value": 4,
+          },
+          {
+                  "name": "Anne",
+                  "value": 2,
+          },
+  ];
+  let ds2 = [
+          {
+                  "name": "Anne",
+                  "value": 5,
+          },
+          {
+                  "name": "Jane",
+                  "value": 3,
+          },
+  ];
 
   let scX = d3.scaleLinear()
       .domain([0, 6])
@@ -18,31 +40,26 @@ function makeDemo() {
       .attr('width', 1200).attr('height', 720);
 
   svg.selectAll('text')
-      .data(ds1, datum => datum[0]).enter().append('text')
+      .data(ds1, datum => datum.name).enter().append('text')
       .attr('x', 150).attr('y', d=>scY(++j))
-      .text(d=>d[0]);
+      .text(d=>d.name);
 
   svg.selectAll('circle')
-      .data(ds1).enter().append('circle')
-      .attr('cx', datum => scX(datum[1]))
+      .data(ds1, datum => datum.name).enter().append('circle')
+      .attr('cx', datum => scX(datum.value))
       .attr('cy', datum => scY(++k))
       .attr('r', 5).attr('fill', 'red');
 
   svg.on('click', function (){
           let cs = svg.selectAll('circle')
-              .data(ds2, datum => datum[0]);
+              .data(ds2, datum => datum.name);
 
           cs.transition().duration(1000)
-              .attr('cx', datum => scX(datum[1]));
+              .attr('cx', datum => scX(datum.value));
 
           cs.exit().attr('fill', 'blue');
 
-
-          console.log(ds2);
-  })
-
-
-  console.log(ds1);
+  });
 }
 
 makeDemo();
